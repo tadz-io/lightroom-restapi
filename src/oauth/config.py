d568ee1307df2fd11f8c2edf479a8aae5ef42976
tadzio <t.holtrop@planblue.com>
2023-10-12 09:09:14 +0100

    config / credentials

---
+++ src/oauth/config.py
@@ -0,0 +1,6 @@
+class Config(object):
+    # Replace below secret key with your secret
+	# Reference: http://flask.pocoo.org/docs/0.12/quickstart/#sessions.
+    FLASK_SECRET = 'PLACEHOLDER_SECRET_KEY'
+    ADOBE_API_KEY = '32d76dde6f664a889931c28bc1016c45'
+    ADOBE_API_SECRET = 'p8e-N4bnTOI0kURJqmmg1PE_w5qDi-FAhUvZ'
